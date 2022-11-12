b370d149660418349a2d6a4660680771aeb5ce5b
Jayesh Patil <jp7470484@gmail.com>
2022-11-12 17:54:01 +0530

    initial commit

---
+++ src/Login.java
@@ -0,0 +1,115 @@
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import java.awt.Font;
+import javax.swing.JPasswordField;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.SwingConstants;
+import javax.swing.ImageIcon;
+import java.awt.Color;
+
+public class Login extends JFrame implements ActionListener{
+
+	private JPanel contentPane;
+	private JTextField textField;
+	private JLabel lblPassword;
+	private JPasswordField passwordField;
+	private JButton btnNewButton;
+	private JButton btnClose;
+	private JButton btnCreateAccount;
+	private JLabel lblWeclomeToHosptial;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Login frame = new Login();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public Login() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 786, 428);
+		contentPane = new JPanel();
+		contentPane.setBackground(new Color(255, 255, 255));
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("Username");
+		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 18));
+		lblNewLabel.setBounds(135, 123, 106, 32);
+		contentPane.add(lblNewLabel);
+		
+		textField = new JTextField();
+		textField.setFont(new Font("Tahoma", Font.PLAIN, 18));
+		textField.setBounds(270, 123, 323, 32);
+		contentPane.add(textField);
+		textField.setColumns(10);
+		
+		lblPassword = new JLabel("Password");
+		lblPassword.setFont(new Font("Tahoma", Font.PLAIN, 18));
+		lblPassword.setBounds(135, 194, 106, 32);
+		contentPane.add(lblPassword);
+		
+		passwordField = new JPasswordField();
+		passwordField.setFont(new Font("Tahoma", Font.PLAIN, 18));
+		passwordField.setBounds(270, 196, 323, 32);
+		passwordField.setColumns(10);
+		contentPane.add(passwordField);
+		
+		btnNewButton = new JButton("Login");
+		btnNewButton.setForeground(new Color(255, 255, 255));
+		btnNewButton.setBackground(new Color(0, 0, 0));
+		btnNewButton.setFont(new Font("Tahoma", Font.PLAIN, 18));
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnNewButton.setBounds(342, 294, 106, 32);
+		contentPane.add(btnNewButton);
+		
+		btnClose = new JButton("Close");
+		btnClose.setForeground(new Color(255, 255, 255));
+		btnClose.setBackground(new Color(0, 0, 0));
+		btnClose.setFont(new Font("Tahoma", Font.PLAIN, 18));
+		btnClose.setBounds(493, 292, 100, 34);
+		contentPane.add(btnClose);
+		btnClose.addActionListener(null);
+		
+		btnCreateAccount = new JButton("Create Account");
+		btnCreateAccount.setForeground(new Color(255, 255, 255));
+		btnCreateAccount.setBackground(new Color(0, 0, 0));
+		btnCreateAccount.setFont(new Font("Tahoma", Font.PLAIN, 18));
+		btnCreateAccount.setBounds(135, 292, 168, 34);
+		contentPane.add(btnCreateAccount);
+		
+		lblWeclomeToHosptial = new JLabel("Weclome to Hosptial Managment System");
+		lblWeclomeToHosptial.setHorizontalAlignment(SwingConstants.CENTER);
+		lblWeclomeToHosptial.setFont(new Font("Trajan Pro", Font.BOLD, 20));
+		lblWeclomeToHosptial.setBounds(107, 46, 522, 32);
+		contentPane.add(lblWeclomeToHosptial);
+	}
+	public void actionPerformed(ActionEvent e){
+		
+	}
+	
+}
